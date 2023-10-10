5576cc4ed097e92d0ff3c3ec96ec580de8397c13
pepszero <135145412+pepszero@users.noreply.github.com>
2023-10-10 03:50:35 +0000

    Set Collection-요구사항1 구현

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,35 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    /**
+     * Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.
+     */
+    @Test
+    @DisplayName("Set의 크기 확인 테스트")
+    void test1() {
+        int size = numbers.size();
+        assertThat(size).isEqualTo(3);
+    }
+
+}
