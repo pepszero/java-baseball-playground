2715ea8390b3362b449df3000a452e4c3321b345
pepszero <135145412+pepszero@users.noreply.github.com>
2023-10-10 03:33:03 +0000

    샘플 - 요구사항1 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,19 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    /**
+     * "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
+     * "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
+     */
+    @Test
+    void test1() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1", "2");
+        assertThat(result).containsExactly("1", "2");
+        assertThat(result[0]).isEqualTo("1");
+    }
+
+    
+
 }
