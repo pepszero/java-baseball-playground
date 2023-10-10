be32eee1a0b6e5d7690387ccdb59295752e17809
pepszero <135145412+pepszero@users.noreply.github.com>
2023-10-10 04:02:33 +0000

    Set Collection-요구사항2 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -8,6 +8,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -32,4 +34,23 @@ public class SetTest {
         assertThat(size).isEqualTo(3);
     }
 
+    /**
+     * Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.
+     * 구현하고 보니 다음과 같이 중복 코드가 계속해서 발생한다.
+     * JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.
+     */
+    // @Test @ParameterizedTest와 중복하여 사용할 수 없음
+    @DisplayName("ParameterizedTest를 이용한 test에 param 넘기기 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 2, 3 }) // 3 numbers
+    void test2(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @Test
+    void contains() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
 }
