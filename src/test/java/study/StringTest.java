dbf4b3b7f430c548eedf5066c85d39e79af566bf
pepszero <135145412+pepszero@users.noreply.github.com>
2023-10-10 03:45:26 +0000

    샘플 - 요구사항3 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -16,6 +18,8 @@ public class StringTest {
      * "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
      */
     @Test
+    @DisplayName("split 테스트")
+
     void test1() {
         String[] result = "1,2".split(",");
         assertThat(result).contains("1", "2");
@@ -27,10 +31,30 @@ public class StringTest {
      * "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.
      */
     @Test
+    @DisplayName("substring 테스트")
+
     void test2() {
         String input = "(1,2)";
         String result = input.substring(1, input.length() - 1);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    /**
+     * "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
+     * String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면
+     * StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.
+     * JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.
+     */
+    @Test
+    @DisplayName("StringIndexOutOfBoundsException 발생 확인")
+    void test3() {
+
+        assertThatThrownBy(() -> {
+            String input = "abc";
+            int index = input.length();
+            input.charAt(index);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("range: 3");
     }
 
 }
