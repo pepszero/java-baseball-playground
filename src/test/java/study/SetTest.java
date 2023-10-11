ccb8fdb7af3cf32c9e8efb165aba3984d45d043c
pepszero <135145412+pepszero@users.noreply.github.com>
2023-10-11 03:40:16 +0000

    Set Collection-요구사항3 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -53,4 +54,20 @@ public class SetTest {
         assertThat(numbers.contains(2)).isTrue();
         assertThat(numbers.contains(3)).isTrue();
     }
+
+    /**
+     * 요구사항 2는 contains 메소드 결과 값이 true인 경우만 테스트 가능하다. 입력 값에 따라 결과 값이 다른 경우에 대한 테스트도
+     * 가능하도록 구현한다.
+     * 예를 들어 1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트를 하나의
+     * Test Case로 구현한다.
+     * 
+     * @param input
+     * @param expected
+     */
+    @ParameterizedTest
+    @CsvSource(value = { "1:true", "2:true", "3:true", "4:false" }, delimiter = ':')
+    void test3(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+
 }
